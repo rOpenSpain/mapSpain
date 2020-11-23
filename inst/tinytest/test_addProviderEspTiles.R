7ac448a6b6ab3744dbce2ffcee66bd1f6f665664
dieghernan <diego.hernangomezherrero@gmail.com>
2020-11-23 19:05:19 +0100

    Test leaflet

---
+++ inst/tinytest/test_addProviderEspTiles.R
@@ -0,0 +1,19 @@
+library(leaflet)
+library(tinytest)
+library(sf)
+
+expect_silent(providerEspTileOptions())
+
+expect_silent(
+
+PuertadelSol <-
+  leaflet() %>%
+  setView(
+    lat = 40.4166,
+    lng = -3.7038400,
+    zoom = 18
+  ) %>%
+  addProviderEspTiles(provider = "IGNBase.Gris") %>%
+  addProviderEspTiles(provider = "RedTransporte.Carreteras")
+
+)
