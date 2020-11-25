8defe3663a1a78173042aa80420c1a45f5e21295
dieghernan <diego.hernangomezherrero@gmail.com>
2020-11-25 08:55:14 +0000

    Try new df

--- data-raw/esp_munic.sf.R
+++ data-raw/esp_munic.sf.R
@@ -1,22 +1,18 @@
 ## code to prepare `esp_munic.sf` dataset goes here
 
-options(gisco_cache_dir = "~/R/mapslib/GISCO")
+install.packages("giscoR")
 
 library(mapSpain)
 library(sf)
 library(giscoR)
 
-df <- esp_get_munic(verbose = TRUE, moveCAN = FALSE, epsg = 4258)
+df <- esp_get_munic(verbose = TRUE, moveCAN = FALSE, epsg = 4258,
+                    update_cache = TRUE)
 plot(df$geometry)
 
 
 
-sf::st_g
-
-
-e <- st_crs(esp_get_ccaa())
-esp_munic.sf <- st_transform(df, st_crs(df))
-esp_munic.sf <- sf::st_make_valid(esp_munic.sf)
+esp_munic.sf <- df
 
 
 
