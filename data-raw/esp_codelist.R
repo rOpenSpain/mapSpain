972c47e7fefc737e8ea5e7aa58df309915b35210
Diego H. <diego.hernangomezherrero@gmail.com>
2025-12-22 23:23:36 +0100

    Maybe encoding in MAC?

--- data-raw/esp_codelist.R
+++ data-raw/esp_codelist.R
@@ -18,9 +18,28 @@ esp_codelist <- nuts1 |>
 
 glimpse(esp_codelist)
 
+# Everything should be UTF-8
+f <- esp_codelist
+ncol <- length(colnames(f))
+
+for (i in seq_len(ncol)) {
+  if (is.character(f[[i]])) {
+    f[[i]] <- enc2utf8(f[[i]])
+    # Check results
+    cli::cli_alert_info(
+      paste0(
+        "In {.str {names(f)[i]}} UTF encodings are ",
+        "{unique(stringi::stri_enc_isutf8(f[[i]]))}"
+      )
+    )
+  }
+}
+
+esp_codelist <- f
+
+
 usethis::use_data(esp_codelist, overwrite = TRUE, compress = "xz")
 tools::checkRdaFiles("./data")
-
 
 # Resave just in case
 write_csv(nuts1, "./data-raw/dict/esp_nuts1.csv", na = "")
@@ -28,4 +47,6 @@ write_csv(ccaa, "./data-raw/dict/esp_ccaa.csv", na = "")
 write_csv(prov, "./data-raw/dict/esp_prov.csv", na = "")
 write_csv(nuts3, "./data-raw/dict/esp_nuts3.csv", na = "")
 
+source("./data-raw/sysdata.R")
+tools::resaveRdaFiles("data")
 rm(list = ls())
